96437fb00c15cd6f132e8771bc82f76ba9e0575a
tanimon <akihitaroron@gmail.com>
2025-05-28 10:43:34 +0900

    0527

--- examtry/test.c
+++ examtry/test.c
@@ -2,9 +2,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+int count_words(char *str)
+{
+    int i;
+    int words;
+    i = 0;
+    words = 0;
+    while(str[i])
+    {
+        while(str[i] == ' ' || str[i] == '\t')
+            i++;
+        if(!str[i])
+            break;
+        words++;
+        while(str[i] && str[i] != ' ' && str[i] != '\t')
+            i++;
+    }
+    return(words);
+}
+
 int	main(void)
 {
-	// int	*i;
-	// i = 10;
-	// printf("%d\n", &*i);
+	char *str = "You hate people! But I love gatherings. Isn't it ironic?";
+	printf("%d\n", count_words(str));
+	return(0);
 }
